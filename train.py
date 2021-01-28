7fd1582e365ae0e2828e9f688251690fbd58a5d9
SimoneMacci0 <78020865+SimoneMacci0@users.noreply.github.com>
2021-01-28 16:42:29 +0100

    Add files via upload

--- train.py
+++ train.py
@@ -4,34 +4,43 @@ import serial
 import sys, os
 import time
 
+
+"""
+ This script can be invoked by command line similarly to the following example:
+ 
+ python train.py 100 5 "0.1,0.5,-0.3;0.2,-0.3,0.5"
+ 
+ - The first argument corresponds to hidden_shape, the parameter controlling the size of the 
+ hidden layer
+ - The second argument is sigma, the parameter which controls the level of non-linearity of
+ the network
+ - The third argument represents the training data. The data must be encapsulated in " "
+ brackets, values of each row separated by a comma and rows must be separated by semicolon.
+ For example "0.1,0.5,-0.3;0.2,-0.3,0.5" corresponds to two consecutive readings of a
+ single sensor. In case of five sensors, one row will be composed of 15 measurements instead
+ of 3.
+ 
+ The script outputs a trained model saved as .pkl format
+ 
+"""
+
 # Import RBF network from separate script
 sys.path.append(os.path.join(sys.path[0], 'lib'))
 from rbfnet import RBFN
     
-"""    
- Load training dataset from previously collected CSV.
- Here the final application will connect to the stream of sensors, send commands to 
- the robot in order to start the movement and collect the corresponding data for 
- training purpose.
- 
-  Parameters to be modified/tuned on demand:
-    - hidden_shape: Size of the RBF's hidden layer
-    - sigma: parameter controlling the level of non-linearity of the network
- 
-"""
-data_train = pd.read_csv(os.path.join(sys.path[0], 'data')+"/train_trajectory.csv",
-                   sep=',', 
-                   header=None,
-                   dtype=np.float, 
-                   na_values="Null").values[1:]
+hidden_shape = int(sys.argv[1])
+sigma = int(sys.argv[2])
+
+data_train_list = sys.argv[3].replace(",", " ")
+data_train = np.squeeze(np.asarray(np.matrix(data_train_list)))
                    
 # Pre-processing of training data
 X_train = data_train
 Xtn = data_train[1:, :]
 Y_train = np.vstack([Xtn, list(Xtn[-1])])
-                   
+   
 # Model creation
-model = RBFN(hidden_shape=100, sigma=5)
+model = RBFN(hidden_shape=hidden_shape, sigma=sigma)
 tStart = time.time()
 
 # Model training
